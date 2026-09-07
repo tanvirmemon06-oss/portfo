f5e57d55fc2b83ed738fe5443027c282a5255881
tanvirmemon06-oss <tanvirmemon06@gmail.com>
2026-09-07 14:58:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// let i =1
+// while(i<=5){
+//     console.log("Hel");
+//     i++
+// }
+let i =6
+do{
+    console.log(i);
+    i++
+}while(i<=5)
+
+// for(let i=10;i>=1;i--){
+//         console.log(i);
+// }
+
